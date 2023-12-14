d25132472f58694df23f82f2d34b025504a8697a
kade7573 <81975757+kade7573@users.noreply.github.com>
2023-12-14 16:45:30 +0900

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,20 +2,20 @@
 
 ::settings
 
-set vhd_path="%UserProfile%\Desktop\temp.vhdx"
-set drive_label="TEMP"
-set drive_letter="Q"
-set /a size_GB = 1
+SET vhd_path="%UserProfile%\Desktop\temp.vhdx"
+SET drive_label="TEMP"
+SET drive_letter="Q"
+SET /a size_GB = 1
 
-set /a partition = 2
-set diskpart_script_path="%cd%\diskpart.txt"
-set diskpart_log_path="%cd%\diskpart.log"
+SET /a partition = 2
+SET diskpart_script_path="%cd%\diskpart.txt"
+SET diskpart_log_path="%cd%\diskpart.log"
 
 ::create diskpart script
 
 IF EXIST %diskpart_script_path% DEL %diskpart_script_path%
 IF EXIST %diskpart_log_path% DEL %diskpart_log_path%
-set /a size_MB = %size_GB% * 1024
+SET /a size_MB = %size_GB% * 1024
 
 ECHO create vdisk file=%vhd_path% maximum=%size_MB% type=expandable > %diskpart_script_path%
 ECHO select vdisk file=%vhd_path% >> %diskpart_script_path%
